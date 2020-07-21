fc9234364b6339fa501e594221bbb7c6fee59198
Justin Gondron <jgondron@nd.edu>
2020-07-21 11:25:56 -0400

    Add dev stack

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 import { App } from '@aws-cdk/core';
 import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
+import { CdkpipelinesDemoStage } from '../lib/cdkpipelines-demo-stage';
 
 const app = new App();
 
+new CdkpipelinesDemoStage(app, 'Dev', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
 new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
   env: { account: '333680067100', region: 'us-east-2' },
 });
