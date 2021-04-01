adf8bbb5a02225d76279fd42e9eb88bf17f7893b
Justin Gondron <jgondron@nd.edu>
2021-04-01 10:57:06 -0400

    Test a change

--- lib/lambda/handler.ts
+++ lib/lambda/handler.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
   return {
-    body: `Hello from a Lambda Function. My backend url is ${process.env.BACKEND_URL ?? 'unknown'}.`,
+    body: `Hello from a Lambda Function. My backend url is ${process.env.BACKEND_URL ?? 'unknown'}. This is a change.`,
     statusCode: 200,
   };
 }
